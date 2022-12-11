636f022fe9b718a61307ee31b27e921b9dedfbc2
Adeola-creator <meetprecious1403@gmail.com>
2022-12-11 20:13:34 +0000

    create login route

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,30 @@
 import { Medic } from "../../models/users/medic";
 import bcrypt from "bcrypt";
-
-export default async function handler (req, res){
-    const {email, password} = req.body
-    await Medic.findOne({email})
-    .then((medic) => {
-        try{
-            bcrypt.compare(password,medic.password)
-            .then((medic) => {
-                if(medic){
-                   const token = jwt.sign
-                }
-            })
-        }catch{}
-    }).catch((error) =>  res.status(404).json({
-        ...error,
-        message:"User not found"
-    }))
-}
+import  jwt from "jsonwebtoken";import connectDB from "../../utils/connectdb";
+export default async function handler(req, res) {connectDB();
+  const {email, password} = req.body;
+  await Medic.findOne({ email }).then((medic) => {
+    if (medic) {
+      // check password
+      bcrypt.compare(password, medic.password, (error, result) => {
+        if (error) {
+          return res.status(401).json({
+            message: "Auth failed, Check your credentials",
+            value: error,
+          });
+        }
+        const token = jwt.sign({
+            active_user: medic.email,
+            id: medic._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: "1h"
+        }, )
+        res.status(200).json({
+          message: "Auth Successful",
+          token,
+          ACK: result,
+        });
+      });
+    }
+  });
+};
