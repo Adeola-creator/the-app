95ca4297f826fea9f365e7d6b8df959bd48f1de6
Adeola-creator <59116131+Adeola-creator@users.noreply.github.com>
2022-12-11 20:32:33 +0000

    import jwt

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,33 @@
 import { Medic } from "../../models/users/medic";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export default async function handler (req, res){
-    const {email, password} = req.body
-    await Medic.findOne({email})
-    .then((medic) => {
-        try{
-            bcrypt.compare(password,medic.password)
-            .then((medic) => {
-                if(medic){
-                   const token = jwt.sign
-                }
-            })
-        }catch{}
-    }).catch((error) =>  res.status(404).json({
-        ...error,
-        message:"User not found"
-    }))
-}
+export default async function handler(req, res) {
+    const {email, password}= req.body;
+    await Medic.findOne({ email }).then((medic) => {
+      console.log(medic);
+      if (medic) {
+        console.log(medic);
+        // check password
+        bcrypt.compare(password, medic.password, (error, result) => {
+          if (error) {
+            return res.status(401).json({
+              message: "Auth failed, Check your credentials",
+              value: error,
+            });
+          }
+          const token = jwt.sign({
+              active_user: medic.email,
+              id: medic._id
+          }, process.env.JWT_SECRET, {
+              expiresIn: "1h"
+          }, )
+          res.status(200).json({
+            message: "Auth Successful",
+            token,
+            ACK: result,
+          });
+        });
+      }
+    });
+  };
