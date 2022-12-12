946684fc30fc1549795203b1fcef65404e187ae8
Adeola <meetprecious1403@gmail.com>
2022-12-12 16:29:43 +0100

    modify api routes

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,30 +1,33 @@
 import { Medic } from "../../models/users/medic";
 import bcrypt from "bcrypt";
-import  jwt from "jsonwebtoken";import connectDB from "../../utils/connectdb";
-export default async function handler(req, res) {connectDB();
-  const {email, password} = req.body;
-  await Medic.findOne({ email }).then((medic) => {
-    if (medic) {
-      // check password
-      bcrypt.compare(password, medic.password, (error, result) => {
-        if (error) {
-          return res.status(401).json({
-            message: "Auth failed, Check your credentials",
-            value: error,
-          });
-        }
-        const token = jwt.sign({
-            active_user: medic.email,
-            id: medic._id
-        }, process.env.JWT_SECRET, {
-            expiresIn: "1h"
-        }, )
-        res.status(200).json({
-          message: "Auth Successful",
-          token,
-          ACK: result,
+import jwt from "jsonwebtoken"; import connectDB from "../../utils/connectdb";
+export default async function handler(req, res) {
+
+    connectDB();
+    const { email, password } = req.body;
+    await Medic.findOne({ email })
+        .then((medic) => {
+            const match = bcrypt.compare(password, medic.password)
+            const token = jwt.sign({ ...medic }, process.env.JWT_SECRET, {
+                expiresIn: 3600
+            })
+            if (match) {
+                res.status(200).json({
+                    message: "Auth Successful",
+                    token,
+                    ACK: match,
+                });
+            } else {
+                return res.status(401).json({
+                    message: "Auth failed, Check your credentials",
+                    value: error,
+                });
+            }
+        })
+        .catch(error => {
+            res.json({
+                ...error,
+                message: "Medic not found"
+            })
         });
-      });
-    }
-  });
 };
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -32,6 +32,7 @@ export default async function handler(req, res) {
                     }
                 })
                 .catch((err) => res.json({ ...err }))
+                break;
     }
 
 }
