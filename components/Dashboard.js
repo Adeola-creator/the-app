c7e0031489f7282d441dcb01095dcd8a3611029f
Adeola <meetprecious1403@gmail.com>
2022-12-17 16:26:57 +0100

    add styling

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 
 function Dashboard() {
-  return <div className='flex w-full justify-between p-5'>
-  <div className='flex flex-col justify-center items center'>
+  return <div className='flex flex-row w-full justify-between p-5 mt-20'>
+  <div className='flex flex-col justify-center md:w-full'>
     <h1 className='text-3xl flex flex-col text-[#007560] py-2'>
-    <span> GPI </span>
+    <span className='text-5xl font-bold'> GPI </span>
     Clinic Management System</h1>
-    <p className='text-xl'>Record Patient information, Vitals and more</p>
-    <button className='bg-[#007560] text-white max-w-[15ch] p-2 my-2 rounded-3xl font-bold'>Proceed  <span className='text-xl font-bold'>&rarr;</span> </button>
+    <p className='text-xl'>Record Patient information, vitals, keep Clinic store, document visits  and more</p>
+    <a href='/patients' className='bg-[#007560] text-white cursor-pointer text-center max-w-[15ch] p-2 my-2 rounded-3xl font-bold'>Proceed <span className='text-xl font-extrabold'>&rarr;</span> </a>
   </div>
   <Image className="rounded-full opacity-50" src='/green.jpg' width={400} height={200}/>
   </div>
