5336fbb160eed57cb5585a8c5e85ebb51122f3be
Adeola <meetprecious1403@gmail.com>
2022-12-17 16:39:06 +0100

    remove api request

--- pages/patients.js
+++ pages/patients.js
@@ -1,22 +1,13 @@
-import { React, useEffect, useState} from "react"
+import { React } from "react"
 import Search from "../components/Search"
-import Card from "../components/Card"
-import { api } from "../utils/api"
+import Box from "../components/Box"
 
- function Patients(){
-   const [patients, setPatients] =useState([])
-   useEffect(() => {
-      api.get("/patients")
-      .then(({data}) => {
-         setPatients(data.data)
-      },[patients])
-   })
-   console.log(patients);
-    return <>
-        <Search />
-        <button className="w-full max-w-[15ch] rounded text-[#007560] font-medium border border-[#007560] p-1 ml-5 mt-5"><a href="/create">Add Patients <i className="fa-regular fa-plus"></i></a></button>
-        {/* {patients.map((patient, index) => <Card key={index}/>)} */}
-        
-    </>
- }
- export default Patients
+
+function Patients() {
+   return <>
+      <Search />
+      <button className="w-full max-w-[15ch] rounded text-[#007560] font-medium border border-[#007560] p-1 ml-5 mt-5"><a href="/create">Add Patients <i className="fa-regular fa-plus"></i></a></button>
+      <Box />
+   </>
+}
+export default Patients
