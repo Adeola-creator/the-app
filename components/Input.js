bd8a10b6265cf3675cfede475fe1b168a6472087
Adeola <meetprecious1403@gmail.com>
2022-12-17 16:33:40 +0100

    refactor code, add onChange handler

--- components/Input.js
+++ components/Input.js
@@ -2,13 +2,15 @@
 import React from "react";
 
 function Input(props) {
+  const {label, type, placeholder, name, value, onChange} = props
   return (
-    <label for={props.for} className="flex flex-col w-full mb-2 font-medium">
-      {props.label}
-      <input type={props.type} 
-      placeholder={props.placeholder}
-      name={props.name}
-      value={props.value}
+    <label className="flex flex-col w-full mb-2 font-medium">
+      {label}
+      <input required type={type} 
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      onChange={onChange}
       className='border  bg-gray-100 rounded outline-none p-1 focus:border-[#006f5b]'/>
     </label>
   );
