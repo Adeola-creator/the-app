47cbeabcdd21848cc77dc38bb48fd7ded6a6465f
Adeola <meetprecious1403@gmail.com>
2022-12-19 16:40:57 +0100

    add state

--- components/Search.js
+++ components/Search.js
@@ -1,8 +1,14 @@
-function Search() {
+import { useState } from "react";
+function Search(props) {
+  const [searchTerm, setSearchTerm] = useState("")
   return (
     <div className="flex bg-gray-50 p-10 w-full justify-center shadow">
-      <input className="w-full max-w-[40ch] p-2 rounded-md text-sm mx-5 outline-none duration-300 hover:opacity-70 hover:mx-5 focus:outline-[#007560] focus:outline-1 shadow-lg" placeholder="Quick Search" type="text" />
-      <button className="w-full max-w-[30ch] bg-[#007560] text-white text-sm text-extrabold  duration-200 hover:bg-white hover:text-[#006f5b] hover:border hover:border-[#006f5b] shadow">Search</button>
+      <input name="searchTerm" 
+      value= {searchTerm} 
+      onChange={(e)=> {setSearchTerm(e.target.value)
+      props.onSearch(searchTerm)
+      }} className="w-full max-w-[60ch] p-3 rounded-md text-center text-sm mx-5 outline-none duration-300 hover:opacity-70 hover:mx-5 focus:outline-[#007560] focus:outline-1 shadow-lg" placeholder="Quick Search" type="text" />
+      {/* <button className="w-full max-w-[30ch] bg-[#007560] text-white text-sm text-extrabold  duration-200 hover:bg-white hover:text-[#006f5b] hover:border hover:border-[#006f5b] shadow">Search</button> */}
     </div>
   );
 }
