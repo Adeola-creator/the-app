9b80d526d7de266a5ccbdc72a8a00380dbbc8d61
Adeola <meetprecious1403@gmail.com>
2022-12-19 16:44:07 +0100

    format file

--- pages/create.js
+++ pages/create.js
@@ -1,47 +1,48 @@
 import axios from "axios";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
 
 function Create() {
-    const [formData, setFromData] =useState({
-        firstName:"",
-        lastName:"",
-        phone:null,
-        address:"",
-        dob:"",
-        gender:"",
-        nextOfKin:"",
-        nextOfKinContact:"",
-        department:"",
-        createdby:"",
-        weight:"",
-        height:"",
+    const [formData, setFromData] = useState({
+        firstName: "",
+        lastName: "",
+        phone: "",
+        address: "",
+        dob: "",
+        gender: "",
+        nextOfKin: "",
+        nextOfKinContact: "",
+        department: "",
+        createdby: "",
+        weight: "",
+        height: "",
         bloodPressure: "",
-        bloodType:"",
-        genotype:"",
-        allergies:""
+        bloodType: "",
+        genotype: "",
+        allergies: ""
     })
-    function handleChange(e){
-        const {name, value} = e.target
+    function handleChange(e) {
+        const { name, value } = e.target
         setFromData(prevValues => ({
             ...prevValues,
-            [name]:value}))
+            [name]: value
+        }))
     }
-    function handleCreate(){
+    function handleCreate() {
         console.log(formData);
-        axios.post('http://localhost:3000/api/patients',{
+        axios.post('http://localhost:3000/api/patients', {
             ...formData
         })
-        .then(res => {
-            alert("Patient created")
-            console.log(res);
-        })
-        .catch(err => console.log(err))
+            .then(res => {
+                alert("Patient created")
+                console.log(res);
+            })
+            .catch(err => console.log(err))
         return;
     }
     return (
         <div className="flex flex-1 flex-col">
-            <form  className="flex flex-col w-full relative select-none p-10">
+            <form className="flex flex-col w-full relative select-none p-10">
                 <h1 className="select-none text-3xl text-[#006f5b] font-medium my-2">Add New Patient</h1>
                 <div className="flex w-full gap-2 md:gap-6">
                     <Input
@@ -94,14 +95,14 @@ function Create() {
                         onChange={handleChange}
                     />
                     <label className="w-full flex flex-col">
-                    Gender
-                    <select name="gender" value={formData.gender} onChange={handleChange} className="border bg-gray-100  outline-none rounded h-10">
-                        <option disabled selected>Select</option>
-                        <option className="hover:bg-[#007560]">Male</option>
-                        <option className="hover:bg-[#007560]">Female</option>
-                    </select>
+                        Gender
+                        <select name="gender" value={formData.gender} onChange={handleChange} className="border bg-gray-100  outline-none rounded h-10">
+                            <option>Select</option>
+                            <option className="hover:bg-[#007560]">Male</option>
+                            <option className="hover:bg-[#007560]">Female</option>
+                        </select>
                     </label>
-                 
+
                 </div>
                 <div className="flex w-full gap-2 md:gap-6">
                     <Input
@@ -124,7 +125,7 @@ function Create() {
                     />
                 </div>
                 <div className="flex w-full gap-2 md:gap-6">
-                <Input
+                    <Input
                         label="Department"
                         placeholder="department"
                         type='text'
@@ -132,7 +133,7 @@ function Create() {
                         for='department'
                         value={formData.department}
                         onChange={handleChange}
-                        
+
                     />
                     <Input
                         label="Attendant Name"
@@ -143,68 +144,68 @@ function Create() {
                         value={formData.createdby}
                         onChange={handleChange}
                     />
-                  
+
                 </div>
                 <h2 className="text-xl my-2 font-semibold text-[#007560]">Personal Information</h2>
                 <div className="flex w-full flex-col">
-                <div className="flex gap-5">
-                <Input
-                        label="Height"
-                        type='text'
-                        name='height'
-                        for='height'
-                        value={formData.height}
-                        onChange={handleChange}
-                    />
-                     <Input
-                        label="Weight"
-                        type='text'
-                        name='weight'
-                        for='weight'
-                        value={formData.weight}
-                        onChange={handleChange}
-                    />
-                   
-                   <Input
-                        label="Blood Pressure"
-                        type='text'
-                        name='bloodPressure'
-                        for='bloodPressure'
-                        value={formData.bloodPressure}
-                        onChange={handleChange}
-                    />
-                </div>
                     <div className="flex gap-5">
-                    <Input
-                        label="Blood Type"
-                        type='text'
-                        name='bloodType'
-                        for='bloodType'
-                        value={formData.bloodType}
-                        onChange={handleChange}
-                    />
-                       <Input
-                        label="Genotype"
-                        type='text'
-                        name='genotype'
-                        for='genotype'
-                        value={formData.genotype}
-                        onChange={handleChange}
-                    />
-                       <Input
-                        label="Allergies"
-                        type='text'
-                        name='allergies'
-                        for='allergies'
-                        value={formData.allergies}
-                        onChange={handleChange}
-                    />
+                        <Input
+                            label="Height"
+                            type='text'
+                            name='height'
+                            for='height'
+                            value={formData.height}
+                            onChange={handleChange}
+                        />
+                        <Input
+                            label="Weight"
+                            type='text'
+                            name='weight'
+                            for='weight'
+                            value={formData.weight}
+                            onChange={handleChange}
+                        />
+
+                        <Input
+                            label="Blood Pressure"
+                            type='text'
+                            name='bloodPressure'
+                            for='bloodPressure'
+                            value={formData.bloodPressure}
+                            onChange={handleChange}
+                        />
                     </div>
-                    
+                    <div className="flex gap-5">
+                        <Input
+                            label="Blood Type"
+                            type='text'
+                            name='bloodType'
+                            for='bloodType'
+                            value={formData.bloodType}
+                            onChange={handleChange}
+                        />
+                        <Input
+                            label="Genotype"
+                            type='text'
+                            name='genotype'
+                            for='genotype'
+                            value={formData.genotype}
+                            onChange={handleChange}
+                        />
+                        <Input
+                            label="Allergies"
+                            type='text'
+                            name='allergies'
+                            for='allergies'
+                            value={formData.allergies}
+                            onChange={handleChange}
+                        />
+                    </div>
+
                 </div>
                 <div className=" flex justify-center mt-10 gap-10">
-                <button onClick={handleCreate} className="max-w-[10ch] text-white bg-[#007560] text-sm font-bold w-20 rounded-3xl p-1"><a>Add <i className="fa-solid fa-user-plus"></i></a></button>
-                <button className="max-w-[10ch] text-[#007560] text-sm font-bold border border-[#007560] w-20 rounded-3xl p-1"><a href="/patients">Cancel</a></button>
+                    <button onClick={handleCreate} className="max-w-[10ch] text-white bg-[#007560] text-sm font-bold w-20 rounded-3xl p-1"><a>Add <i className="fa-solid fa-user-plus"></i></a></button>
+                    <button className="max-w-[10ch] text-[#007560] text-sm font-bold border border-[#007560] w-20 rounded-3xl p-1"><a href="/patients">Cancel</a></button>
                 </div>
             </form>
         </div>
