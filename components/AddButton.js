aa0a48bc84bd272733b2f0a4d7b8cfe5b0f60642
Adeola-creator <meetprecious1403@gmail.com>
2023-01-05 05:55:43 +0100

    add style

--- components/AddButton.js
+++ components/AddButton.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function AddButton(props) {
   return (
-    <Link className="w-full max-w-[15ch] rounded text-[#007560] font-medium border border-[#007560] p-1 ml-5 mt-5" href={props.route}>
+    <Link className="w-full max-w-[15ch] rounded text-[#007560] text-center font-medium border border-[#007560] p-1 ml-5 mt-5" href={props.route}>
     {props.title} <i className="fa-regular fa-plus"></i>
   </Link>
   )
