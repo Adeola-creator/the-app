344fd6883c01b13bb7270b81f29504867cceb49a
Adeola-creator <meetprecious1403@gmail.com>
2023-01-26 11:41:26 +0100

    add styles

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -2,8 +2,8 @@ import Link from "next/link"
 import { FaTimes } from "react-icons/fa"
 function MobileNav({showNav, active}){
     return(
-        <ul className={active ? "flex flex-col gap-10 items-center text-xl font-bold fixed bg-black/5 backdrop-blur-sm inset-0 justify-center left-1/4 md:hidden" : "hidden"}>
-            <FaTimes className="absolute top-7 right-5 text-3xl" onClick={showNav}/>
+        <ul  onClick={showNav} className={active ? "flex flex-col gap-10 items-center z-5 text-2xl font-bold fixed bg-black/10 backdrop-blur-sm inset-0 justify-center left-1/4 md:hidden" : "hidden"}>
+            <FaTimes className="absolute top-7 right-5 text-3xl hover:text-red-700 cursor-pointer" onClick={showNav}/>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/patients">Patients</Link></li>
             <li><Link href="/visits">Visits</Link></li>
